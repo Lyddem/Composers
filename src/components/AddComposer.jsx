6378e9c319714d97c0534f5c42f839ce91c9f303
Lyddem <lydiademi@gmail.com>
2017-10-23 20:33:46 -0700

    Add redirect to home page on submit click

--- src/components/AddComposer.jsx
+++ src/components/AddComposer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class AddComposer extends Component {
   constructor(props) {
     super(props)
+  }
+
+  clicked() {
+    console.log('clicked!');
   }
 
   render () {
@@ -12,7 +17,7 @@ class AddComposer extends Component {
         <div id="CTA">
           Name <input type="text" /> <br /><br /><br />
           Image Url <input type="text" /> <p />
-          <button type="submit">Submit</button>
+          <Link to="/"><button type="submit" onClick={this.clicked.bind(this)}>Submit</button></Link>
         </div>
       </div>
     )
