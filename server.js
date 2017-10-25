5227f687194121fe87ca8e1533304ca5dfe49970
Lyddem <lydiademi@gmail.com>
2017-10-24 17:55:53 -0700

    Cleanup

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const mysql = require('mysql');
-const app = express();
 const PORT = 3000 || PROCESS.ENV.PORT;
 const bodyParser = require('body-parser');
 const path = require('path');
-// const db = require('./database/index.js');
+const mysql = require('mysql');
 
+const express = require('express');
+const app = express();
+
+//serve static files
+app.use(express.static('dist'));
+app.use(bodyParser.json());
+
+//database connection
 const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'YES',
@@ -13,14 +18,7 @@ const connection = mysql.createConnection({
   database: 'composers'
 })
 
-app.use(bodyParser.json());
-
-app.use(express.static('dist'));
-
-// app.get('/', () => {
-
-// })
-
+//Server setup with express -- see lines 6 & 7
 app.listen(3000, () => {
   console.log('listening on port ' + PORT);
 })
